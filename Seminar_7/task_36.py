c110a36fcf540c4191ad4726e8b4cf5acee1652e
Sergey <samtriezo@gmail.com>
2023-08-11 22:22:12 +0700

    сделал задачу 36

--- Seminar_7/task_36.py
+++ Seminar_7/task_36.py
@@ -3,3 +3,12 @@
 # Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля).
 # Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например, у операции умножения.
 
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    #print([operation(x, y) for x in range(1,num_rows+1) for y in range(1,num_columns+1)]) # list comprehension, только не знаю как вывести таблицей без доп. циклов...
+    for x in range(1,num_rows+1):
+        
+        for y in range(1,num_columns+1):
+            print("{:3d}".format(operation(x, y)), end=" ")
+        print()
+
+print_operation_table(lambda x, y: x * y)
